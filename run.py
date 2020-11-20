d84c9f0e19bf12ccf90948eb5c62c2a41b63e265
Sadiq <sadiq.kassamali@gmail.com>
2020-11-19 23:37:49 -0600

    Dropping last column

--- run.py
+++ run.py
@@ -10,12 +10,13 @@ if (len(d) > 0) :
     print(len(d))
     print("print total data before NaN removal: ")
     print(d)
-    d.fillna('', inplace=True)
-    print("print total data after NaN removal: " )
-    print(d)
-    d.drop('Unnamed: 3', axis=1)
-    print(d)
-    #print(d.head()) #if you want to view first n-rows
-    #print(d.tail())  # if you want to view last  n-rows
-    #print(d[20:30 + 1])  #slice and dice of rows from 20 : 30
-    #print(d.describe()) #Statistics of your dataframe if value is int
+    #d.fillna('', inplace=True)
+    # print("print total data after NaN removal: " )
+    #print(d)
+    dropped_column = d.drop(d.columns[-1],axis=1)  # Drop last column
+    print("print total data After last column removal: ")
+    print(dropped_column)
+    #print(dropped_column.head()) #if you want to view first n-rows
+    #print(dropped_column.tail())  # if you want to view last  n-rows
+    #print(dropped_column[20:30 + 1])  #slice and dice of rows from 20 : 30
+    print(dropped_column.describe()) #Statistics of your dataframe if value is int
