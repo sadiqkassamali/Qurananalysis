8808c7742f5dddcda661df59893cf7ea31cab8a0
Sadiq <sadiq.kassamali@gmail.com>
2020-11-19 23:38:48 -0600

    removing usless prints

--- run.py
+++ run.py
@@ -8,8 +8,8 @@ d=pd.read_csv('data\quran.csv')
 if (len(d) > 0) :
     print("print total rows count: ")
     print(len(d))
-    print("print total data before NaN removal: ")
-    print(d)
+    #print("print total data before NaN removal: ")
+    #print(d)
     #d.fillna('', inplace=True)
     # print("print total data after NaN removal: " )
     #print(d)
@@ -19,4 +19,4 @@ if (len(d) > 0) :
     #print(dropped_column.head()) #if you want to view first n-rows
     #print(dropped_column.tail())  # if you want to view last  n-rows
     #print(dropped_column[20:30 + 1])  #slice and dice of rows from 20 : 30
-    print(dropped_column.describe()) #Statistics of your dataframe if value is int
+    #print(dropped_column.describe()) #Statistics of your dataframe if value is int
