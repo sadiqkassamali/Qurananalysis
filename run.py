f954b11ba55cb493d97c69af030d09dbaf672475
Sadiq <sadiq.kassamali@gmail.com>
2020-12-24 05:50:02 -0600

    Updates for plots and README.md

--- run.py
+++ run.py
@@ -6,24 +6,23 @@ from plotter import print_similar_word_cloud
 
 logging.root.level = logging.INFO
 from IPython.core.interactiveshell import InteractiveShell
-
 InteractiveShell.ast_node_interactivity = "all"
 import matplotlib
 
 matplotlib.use('TkAgg')
+import numpy as np
 
-'''
-#Scatter plott of all words.
+# Scatter plott of all words.
+plt.figure(1)
 circle = plt.Circle((0, 0), radius=1, fc='w')
 plt.gca().add_patch(circle)
 plt.xlim([-3, 3])
 plt.ylim([-3, 3])
-plt.yticks(pd.np.arange(-3, 3, .25))
-plt.xticks(pd.np.arange(-3, 3, .25))
-plt.show()
-'''
+plt.yticks(np.arange(-3, 3, .5))
+plt.xticks(np.arange(-3, 3, .5))
+plt.show
 # Start using the model and play with visualization
-plt.figure(1)
+plt.figure(2)
 plt.subplot(211)
 # Search for work Allah and something that's closest
 word = 'allah'
